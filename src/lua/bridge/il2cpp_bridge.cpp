08e7c1b8b2a11831117b88e7f0292e909738d6fb
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-05-01 23:44:36 +0200

    Added Assembly & Il2cpp mt

--- src/lua/bridge/il2cpp_bridge.cpp
+++ src/lua/bridge/il2cpp_bridge.cpp
@@ -2,6 +2,7 @@
 #include "lualib.h"
 #include "lua.h"
 #include "../../utils/console/console.hpp"
+#include <UnityResolve/UnityResolve.hpp>
 
 #define ASSEMBLY_MT_NAME "assembly.mt"
 #define IL2CPP_MT_NAME "il2cpp.mt"
@@ -12,16 +13,15 @@ int newindex_assembly(lua_State* L)
 
 int index_assembly(lua_State *L)
 {
-    Console::get()->log("-1 -> ", luaL_typename(L, -1));
-    Console::get()->log("-2 -> ", luaL_typename(L, -2));
-
+    if (lua_islightuserdata(L, -2))
+    {
+        auto assembly = (UnityResolve::Assembly*)lua_tolightuserdata(L, -2);
+        lua_pushstring(L, assembly->name.c_str());
+        return 1;
+    }
     return 0;
 }
 
-int new_assembly(lua_State* L)
-{
-    return 1;
-}
 static const luaL_Reg assembly_m[]
 {
     {"__newindex", newindex_assembly}, {"__index", index_assembly}, {NULL, NULL}
@@ -33,12 +33,43 @@ int newindex_il2cpp(lua_State* L)
 }
 int index_il2cpp(lua_State* L)
 {
+    Console::get()->log("-1 -> ", lua_tostring(L, -1));
+    Console::get()->log("-2 -> ", luaL_typename(L, -2));
+    const char* key = luaL_checkstring(L, -1);
+    if (strcmp(key, "get") == 0) {
+        luaL_getmetatable(L, IL2CPP_MT_NAME);
+        lua_getfield(L, -1, "get");
+        return 1;
+    } else if (strcmp(key, "assembly") == 0)
+    {
+        int idx=1;
+        lua_newtable(L);
+        for (auto& pAssembly : UnityResolve::assembly)
+        {
+            lua_pushlightuserdata(L, pAssembly);
+            luaL_getmetatable(L, ASSEMBLY_MT_NAME);
+            lua_setmetatable(L, -2);
+            lua_rawseti(L, -2, idx);
+            // lua setfield ?
+            idx++;
+        }
+        return 1;
+    }
     return 0;
 }
-    
+
+int get_il2cpp(lua_State* L)
+{
+    const char* assembly_name = luaL_checkstring(L, -1);
+    Console::get()->log("Assembly name -> ", assembly_name);
+    lua_pushlightuserdata(L, UnityResolve::Get(assembly_name));
+    luaL_getmetatable(L, ASSEMBLY_MT_NAME);
+    lua_setmetatable(L, -2);
+    return 1;
+}
 static const luaL_Reg il2cpp_m[] =
 {
-    {"__newindex", newindex_il2cpp}, {"__index", index_il2cpp}, {NULL, NULL}
+    {"__newindex", newindex_il2cpp}, {"__index", index_il2cpp},{"get", get_il2cpp}, {NULL, NULL}
 };
 
 void create_lua_mt(lua_State* L, const char* mt_name, const luaL_Reg* m_reg)
@@ -56,9 +87,10 @@ void create_lua_mt(lua_State* L, const char* mt_name, const luaL_Reg* m_reg)
 int lua_bindings::register_il2cpp_bridge(lua_State* L)
 {
     create_lua_mt(L, ASSEMBLY_MT_NAME, assembly_m);
+    create_lua_mt(L, IL2CPP_MT_NAME, il2cpp_m);
     lua_newtable(L);
-    luaL_getmetatable(L, ASSEMBLY_MT_NAME);
+    luaL_getmetatable(L, IL2CPP_MT_NAME);
     lua_setmetatable(L, -2);
-    lua_setglobal(L, "assembly");
+    lua_setglobal(L, "il2cpp");
     return 0;
 }

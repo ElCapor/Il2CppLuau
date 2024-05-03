afb239dc9abfbc876935d6931ee0002f4139e5c2
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-05-03 14:02:21 +0200

    Revert "Added classes"
    This reverts commit 1fb6d6c75b86fe51c7bfd7d13f7e4c9a805066f0.

--- src/lua/bridge/il2cpp_bridge.cpp
+++ src/lua/bridge/il2cpp_bridge.cpp
@@ -6,85 +6,25 @@
 
 #define ASSEMBLY_MT_NAME "assembly.mt"
 #define IL2CPP_MT_NAME "il2cpp.mt"
-#define CLASS_MT_NAME "class.mt"
-#define FIELD_MT_NAME "field.mt"
-#define METHOD_MT_NAME "method.mt"
-
-int newindex_class(lua_State* L)
-{
-    return 0;
-}
-
-int index_class(lua_State* L)
-{
-    const char* key = lua_tostring(L, -1);
-    if (lua_isuserdata(L, -2))
-    {
-        if (strcmp(key, "name") == 0)
-        {
-            auto cls = (UnityResolve::Class*)lua_tolightuserdata(L, -2);
-            lua_pushstring(L, cls->name.c_str());
-            return 1;
-        }
-    }
-    return 0;
-}
-
-static const luaL_Reg class_m[] = {
-    {"__index", index_class}, {"__newindex", newindex_class},{NULL, NULL}
-};
-
 int newindex_assembly(lua_State* L)
 {   
     return 0;
 }
 
 int index_assembly(lua_State *L)
-{   
-    const char* key = lua_tostring(L, -1);
+{
     if (lua_islightuserdata(L, -2))
     {
         auto assembly = (UnityResolve::Assembly*)lua_tolightuserdata(L, -2);
-        if (strcmp(key, "name") == 0)
-        {
-            if (assembly)
-            {
-                lua_pushstring(L, assembly->name.c_str());
-                return 1;
-            } else {
-                return 0;
-            }
-        } else if (strcmp(key, "classes") == 0)
-        {
-            
-            int idx=1;
-            lua_newtable(L);
-            for (auto& pClass : assembly->classes)
-            {
-                lua_pushlightuserdata(L, pClass);
-                luaL_getmetatable(L, CLASS_MT_NAME);
-                lua_setmetatable(L, -2);
-                lua_rawseti(L, -2, idx);
-                idx++;
-            }
-            return 1;
-        }
+        lua_pushstring(L, assembly->name.c_str());
+        return 1;
     }
-    return 0;
-}
-
-int get_assembly(lua_State* L)
-{
-    Console::get()->log("-1 -> ", luaL_typename(L, -1));
-    Console::get()->log("-2 -> ", luaL_typename(L, -2));
-
-
     return 0;
 }
 
 static const luaL_Reg assembly_m[]
 {
-    {"__newindex", newindex_assembly}, {"__index", index_assembly},{"get", get_assembly}, {NULL, NULL}
+    {"__newindex", newindex_assembly}, {"__index", index_assembly}, {NULL, NULL}
 };
 
 int newindex_il2cpp(lua_State* L)
@@ -93,6 +33,8 @@ int newindex_il2cpp(lua_State* L)
 }
 int index_il2cpp(lua_State* L)
 {
+    Console::get()->log("-1 -> ", lua_tostring(L, -1));
+    Console::get()->log("-2 -> ", luaL_typename(L, -2));
     const char* key = luaL_checkstring(L, -1);
     if (strcmp(key, "get") == 0) {
         luaL_getmetatable(L, IL2CPP_MT_NAME);
@@ -108,6 +50,7 @@ int index_il2cpp(lua_State* L)
             luaL_getmetatable(L, ASSEMBLY_MT_NAME);
             lua_setmetatable(L, -2);
             lua_rawseti(L, -2, idx);
+            // lua setfield ?
             idx++;
         }
         return 1;
@@ -144,7 +87,6 @@ void create_lua_mt(lua_State* L, const char* mt_name, const luaL_Reg* m_reg)
 int lua_bindings::register_il2cpp_bridge(lua_State* L)
 {
     create_lua_mt(L, ASSEMBLY_MT_NAME, assembly_m);
-    create_lua_mt(L, CLASS_MT_NAME, class_m);
     create_lua_mt(L, IL2CPP_MT_NAME, il2cpp_m);
     lua_newtable(L);
     luaL_getmetatable(L, IL2CPP_MT_NAME);

ac6e60ef43e6deb42355cc30f6c4325e311d1975
Hamdi Moubrak <104172250+ElCapor@users.noreply.github.com>
2024-05-08 08:12:03 +0200

    idk fr

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -8,17 +8,69 @@
 #include <luau/VM/src/lobject.h>
 #include <luau/VM/src/lapi.h>
 #include <luau/VM/src/lstate.h>
+#include <algorithm>
+template<typename T>
+static T read(uint8_t* data, size_t& offset)
+{
+
+    T result;
+    memcpy(&result, data + offset, sizeof(T));
+    offset += sizeof(T);
+
+    return result;
+}
+
+static uint32_t readVarInt(uint8_t* data, size_t& offset)
+{
+    uint32_t result = 0;
+    uint32_t shift = 0;
+
+    uint8_t byte;
+
+    do
+    {
+        byte = read<uint8_t>(data, offset);
+        result |= (byte & 127) << shift;
+        shift += 7;
+    } while (byte & 128);
+
+    return result;
+}
+
+
 int proto_inspector(lua_State* L)
 {
     if (lua_isLfunction(L, -1))
     {
         const TValue* func = luaA_toobject(L, -1);
         Proto* proto = clvalue(func)->l.p;
-        Console::get()->log("Proto : ", proto->numparams);
-        for (int i = 0; i<proto->sizetypeinfo; i++)
+        uint8_t* data = proto->typeinfo;
+        if (data != nullptr)
         {
-            Console::get()->log("Typeinfo ", proto->typeinfo[i]);
+            size_t offset = 0;
+
+            uint32_t typeSize = readVarInt(data, offset);
+            uint32_t upvalCount = readVarInt(data, offset);
+            uint32_t localCount = readVarInt(data, offset);
+
+            Console::get()->log("Proto ", typeSize, " upval ", upvalCount, " locals ", localCount);
+            std::vector<uint8_t> argumentTypes;
+            uint8_t* types = (uint8_t*)data + offset;
+
+            argumentTypes.resize(proto->numparams);
+
+            // Skip two bytes of function type introduction
+            memcpy(argumentTypes.data(), types + 2, proto->numparams);
+            offset += typeSize;
+            for (auto& tp : argumentTypes)
+            {
+                Console::get()->log("tp : ", tp);
+            }
+
+
+
         }
+        
     }
     return 0;
 }
